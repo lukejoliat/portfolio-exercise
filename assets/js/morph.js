8879e0de1c36948bfc25ef9dead9edce0cb13581
luke.joliat <luke.joliat@slalom.com>
2021-01-04 22:30:33 -0500

    toggle

--- assets/js/morph.js
+++ assets/js/morph.js
@@ -1,25 +1,38 @@
 import '../anime-master/lib/anime.es.js';
 import anime from '../anime-master/lib/anime.es.js';
 
-const moonPath = "M12.5 25C12.5 38.8071 25 50 25 50C11.1929 50 0 38.8071 0 25C0 11.1929 11.1929 0 25 0C25 0 12.5 11.1929 12.5 25Z";
-const sunPath = "M50 25C50 38.8071 38.8071 50 25 50C11.1929 50 0 38.8071 0 25C0 11.1929 11.1929 0 25 0C38.8071 0 50 11.1929 50 25Z";
+const moonPath = "M21.5 25.1202C21.5 31.1608 25.1202 36.0577 25.1202 36.0577C19.0796 36.0577 14.1827 31.1608 14.1827 25.1202C14.1827 19.0796 19.0796 14.1827 25.1202 14.1827C25.1202 14.1827 21.5 19.0796 21.5 25.1202Z";
+const sunPath = "M36.0577 25.1202C36.0577 31.1608 31.1608 36.0577 25.1202 36.0577C19.0796 36.0577 14.1827 31.1608 14.1827 25.1202C14.1827 19.0796 19.0796 14.1827 25.1202 14.1827C31.1608 14.1827 36.0577 19.0796 36.0577 25.1202Z";
 
 const moon = document.querySelector('.moon');
-let path = moonPath;
+let toggle = true;
 
 moon.addEventListener('click', () => {
-  path = path === moonPath ? sunPath : moonPath;
+  const path = toggle === true ? moonPath : sunPath;
+  const fill = path === moonPath ? '#333' : '#EFC83E';
+  const rotate = toggle === true ? 320 : 0;
+  toggle = !toggle;
   const timeline = anime.timeline({
     duration: 750,
     easing: "easeOutExpo"
   });
 
-  timeline.add({
-    targets: '.moon path',
-    d: [
-      { value: path }
-    ]
-  })
+  timeline
+    .add({
+      targets: '.moon .disc',
+      d: [
+        { value: path }
+      ]
+    })
+    .add({
+      targets: '.moon .ray',
+      fill
+    },
+      "-= 750")
+    .add({
+      targets: '.moon',
+      rotate
+    }, "-= 500");
 
   // change colors
   let root = document.documentElement;
